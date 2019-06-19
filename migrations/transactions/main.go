de46df4925837bf2611b2112d50efc98ee10f070
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-06-19 16:23:30 -0400

    Implement tx migration

---
+++ migrations/transactions/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/alexanderbez/juno/client"
+	"github.com/alexanderbez/juno/config"
+	"github.com/alexanderbez/juno/db"
+	"github.com/pkg/errors"
+)
+
+var configPath string
+
+func main() {
+	flag.StringVar(&configPath, "config", "", "Configuration file")
+	flag.Parse()
+
+	cfg := config.ParseConfig(configPath)
+	cl, err := client.New(cfg.RPCNode, cfg.RESTNode)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "failed to start RPC client"))
+	}
+
+	defer cl.Stop() // nolint: errcheck
+
+	db, err := db.OpenDB(cfg)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "failed to open database connection"))
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to ping database"))
+	}
+
+	lastHeight, err := db.LastBlockHeight()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "failed to get latest block height"))
+	}
+
+	for i := int64(2); i < lastHeight; i++ {
+		block, err := cl.Block(i)
+		if err != nil {
+			log.Printf("failed to get block %d: %s", i, err)
+			continue
+		}
+
+		for _, tmTx := range block.Block.Txs {
+			txHash := fmt.Sprintf("%X", tmTx.Hash())
+
+			tx, err := cl.Tx(txHash)
+			if err != nil {
+				log.Printf("failed to get tx %s: %s", txHash, err)
+				continue
+			}
+
+			if i%10 == 0 {
+				log.Printf("migrated transaction %s\n", txHash)
+			}
+
+			if _, err := db.SetTx(tx); err != nil {
+				log.Printf("failed to persist transaction %s: %s", txHash, err)
+			}
+		}
+	}
+}
