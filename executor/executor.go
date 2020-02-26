5090d8b6c7f9fb6e6d1f970b1b470f90395332a6
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-02-26 14:09:05 +0100

    Moved the executor into an external package (fixes #2)

---
+++ executor/executor.go
@@ -0,0 +1,50 @@
+package executor
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/desmos-labs/juno/config"
+	"github.com/desmos-labs/juno/db"
+	"github.com/desmos-labs/juno/parse"
+	"github.com/desmos-labs/juno/types"
+	"github.com/desmos-labs/juno/version"
+	"github.com/spf13/cobra"
+	"github.com/tendermint/tendermint/libs/cli"
+)
+
+// Utility method that allows to build an Executor containing a root command that
+// has the provided name and description.
+//
+// The provided setupCfg method will be used to customize the SDK configuration. If you don't want any customization
+// you can use the types.EmptySetup method.
+//
+// The provided cdcBuilder is used to provide a codec that will later be used to deserialize the
+// transaction messages. Make sure you register all the types you need properly.
+//
+// The provided dbBuilder is used to provide the database that will be used to save the data.
+func BuildExecutor(name string, setupCfg types.SdkConfigSetup, cdcBuilder types.CodecBuilder, dbBuilder db.Builder) cli.Executor {
+
+	sdkConfig := sdk.GetConfig()
+	setupCfg(sdkConfig)
+	sdkConfig.Seal()
+
+	rootCmd := &cobra.Command{
+		Use:   name,
+		Short: fmt.Sprintf("%s is a Cosmos Hub data aggregator and exporter", name),
+		Long: fmt.Sprintf(`A cosmos Hub data aggregator. It improves the Hub's data accessibility
+by providing an indexed database exposing aggregated resources and
+models such as blocks, validators, pre-commits, transactions, and various aspects
+of the governance module. %s is meant to run with a GraphQL layer on top so that
+it even further eases the ability for developers and downstream clients to answer
+queries such as "what is the average gas cost of a block?" while also allowing
+them to compose more aggregate and complex queries.`, name),
+	}
+
+	rootCmd.AddCommand(
+		version.GetVersionCmd(),
+		parse.GetParseCmd(cdcBuilder(), dbBuilder),
+	)
+
+	return config.PrepareMainCmd(rootCmd)
+}
