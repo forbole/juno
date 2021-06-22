8862204c82fc857b3663a8e7db93d6129ed8c5f3
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-06-22 12:56:11 +0200

    Added more gRPC utils inside the client package

--- client/utils.go
+++ client/utils.go
@@ -1,10 +1,31 @@
 package client
 
 import (
-	"google.golang.org/grpc"
+	"strconv"
 
 	"github.com/desmos-labs/juno/types"
+
+	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
+
+// GetHeightRequestHeader returns the grpc.CallOption to query the state at a given height
+func GetHeightRequestHeader(height int64) grpc.CallOption {
+	header := metadata.New(map[string]string{
+		grpctypes.GRPCBlockHeightHeader: strconv.FormatInt(height, 10),
+	})
+	return grpc.Header(&header)
+}
+
+// MustCreateGrpcConnection creates a new gRPC connection using the provided configuration and panics on error
+func MustCreateGrpcConnection(cfg types.Config) *grpc.ClientConn {
+	grpConnection, err := CreateGrpcConnection(cfg)
+	if err != nil {
+		panic(err)
+	}
+	return grpConnection
+}
 
 // CreateGrpcConnection creates a new gRPC client connection from the given configuration
 func CreateGrpcConnection(cfg types.Config) (*grpc.ClientConn, error) {
