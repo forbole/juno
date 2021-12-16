4ab6d75d38b015647918a85461584bd322dbda0e
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2021-12-16 09:51:17 +0000

    feat: add sealed config check (#34)
    <!-- < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < ☺
    v                               ✰  Thanks for creating a PR! ✰    
    v    Before smashing the submit button please review the checkboxes.
    v    If a checkbox is n/a - please still include it but + a little note why
    ☺ > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >  -->
    
    ## Description
    <!-- Small description -->
    
    ## Checklist
    - [x] Targeted PR against correct branch.
    - [ ] Linked to Github issue with discussion and accepted design OR link to spec that describes this work.
    - [ ] Wrote unit tests.  
    - [x] Re-reviewed `Files changed` in the Github PR explorer.

--- cmd/parse/setup.go
+++ cmd/parse/setup.go
@@ -11,6 +11,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	modsregistrar "github.com/forbole/juno/v2/modules/registrar"
+
+	"context"
 )
 
 // GetParsingContext setups all the things that should be later passed to StartParsing in order
@@ -18,14 +20,23 @@ import (
 func GetParsingContext(parseConfig *Config) (*Context, error) {
 	// Get the global config
 	cfg := config.Cfg
+	var sdkConfig *sdk.Config
 
 	// Build the codec
 	encodingConfig := parseConfig.GetEncodingConfigBuilder()()
 
-	// Setup the SDK configuration
-	sdkConfig := sdk.GetConfig()
-	parseConfig.GetSetupConfig()(cfg, sdkConfig)
-	sdkConfig.Seal()
+	sealedCfg, err := sdk.GetSealedConfig(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	// If sealedCfg is null it means the config has not been sealed yet
+	if sealedCfg == nil {
+		// Setup the SDK configuration
+		sdkConfig = sdk.GetConfig()
+		parseConfig.GetSetupConfig()(cfg, sdkConfig)
+		sdkConfig.Seal()
+	}
 
 	// Get the db
 	databaseCtx := database.NewContext(cfg.Database, &encodingConfig, parseConfig.GetLogger())
@@ -52,8 +63,8 @@ func GetParsingContext(parseConfig *Config) (*Context, error) {
 	}
 
 	// Get the modules
-	context := modsregistrar.NewContext(cfg, sdkConfig, &encodingConfig, db, cp, parseConfig.GetLogger())
-	mods := parseConfig.GetRegistrar().BuildModules(context)
+	moduleContext := modsregistrar.NewContext(cfg, sdkConfig, &encodingConfig, db, cp, parseConfig.GetLogger())
+	mods := parseConfig.GetRegistrar().BuildModules(moduleContext)
 	registeredModules := modsregistrar.GetModules(mods, cfg.Chain.Modules, parseConfig.GetLogger())
 
 	return NewContext(&encodingConfig, cp, db, parseConfig.GetLogger(), registeredModules), nil
