2d842a55099e0a6e3778f5d2cd547f3d19d2b487
huichiaotsou <huichiao.tsou@gmail.com>
2022-03-01 13:29:57 +0800

    add start and end heights

--- cmd/fix/blocks/blocks.go
+++ cmd/fix/blocks/blocks.go
@@ -12,7 +12,9 @@ import (
 )
 
 const (
-	flagForce = "force"
+	flagForce       = "force"
+	flagStartHeight = "start"
+	flagEndHeight   = "end"
 )
 
 // blocksCmd returns a Cobra command that allows to fix missing blocks in database
@@ -21,6 +23,7 @@ func blocksCmd(parseConfig *parse.Config) *cobra.Command {
 		Use:   "all",
 		Short: "Fix missing blocks and transactions in database from the start height",
 		RunE: func(cmd *cobra.Command, args []string) error {
+
 			parseCtx, err := parse.GetParsingContext(parseConfig)
 			if err != nil {
 				return err
@@ -36,9 +39,15 @@ func blocksCmd(parseConfig *parse.Config) *cobra.Command {
 			}
 
 			force, _ := cmd.Flags().GetBool(flagForce)
+			startHeight, _ := cmd.Flags().GetInt64(flagStartHeight)
+			endHeight, _ := cmd.Flags().GetInt64(flagEndHeight)
 
-			k := config.Cfg.Parser.StartHeight
-			fmt.Printf("Refetching missing blocks and transactions from height %d... \n", k)
+			k := startHeight
+			if endHeight > 0 {
+				height = endHeight
+			}
+
+			fmt.Printf("Refetching missing blocks and transactions from height %d to %d \n", k, height)
 			for ; k <= height; k++ {
 				if force {
 					err = worker.Process(k)
@@ -56,6 +65,8 @@ func blocksCmd(parseConfig *parse.Config) *cobra.Command {
 	}
 
 	cmd.Flags().Bool(flagForce, false, "If set, forces the fetch of blocks by overwriting any existing one")
+	cmd.Flags().Int64(flagStartHeight, config.Cfg.Parser.StartHeight, "If set, parses from the configured height")
+	cmd.Flags().Int64(flagEndHeight, 0, "If set, parses up to the configured height")
 
 	return cmd
 }
