bc8bedcfd3973a9f431c21c00f23150a657b86e3
Aaron <76254323+huichiaotsou@users.noreply.github.com>
2022-03-02 10:54:44 +0800

    Update cmd/fix/blocks/blocks.go
    Co-authored-by: Magic Cat <37407870+MonikaCat@users.noreply.github.com>

--- cmd/fix/blocks/blocks.go
+++ cmd/fix/blocks/blocks.go
@@ -71,7 +71,7 @@ func blocksCmd(parseConfig *parse.Config) *cobra.Command {
 
 cmd.Flags().Bool(flagForce, false, "If set to true forces bdjuno to refetch all blocks from given height including overwriting any existing ones in database (default false)")
 	cmd.Flags().Int64(flagStart, 0, "Set the height to start refetching missing blocks from")
-	cmd.Flags().Int64(flagEnd, 0, "Set the height to which the refetching finishes")
+	cmd.Flags().Int64(flagEnd, 0, "Set the height to finish refetching missing blocks at")
 
 	return cmd
 }
