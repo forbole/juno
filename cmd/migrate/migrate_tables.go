adfd425a7574900d6285f7e3b19d1c0be9808755
Magic Cat <pusz.monika@gmail.com>
2022-03-04 18:28:49 +0000

    updated MigrateTablesCmd

--- cmd/migrate/migrate_tables.go
+++ cmd/migrate/migrate_tables.go
@@ -1,25 +1,43 @@
 package migrate
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/forbole/juno/v2/types/config"
+	"github.com/forbole/juno/v2/cmd/parse"
+	database "github.com/forbole/juno/v2/database"
+
+
 )
 
 // MigrateTablesCmd returns a Cobra command that allows to implement table partition
-func MigrateTablesCmd() *cobra.Command {
+func MigrateTablesCmd(parseConfig *parse.Config) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "migrate-tables",
 		Short: "Migrate transaction and message tables to tables with partition",
 		RunE: func(cmd *cobra.Command, args []string) error {
 	
-		limit := db.Config.Database.Limit
+		// Get config		
+		cfg := config.Cfg
+
+		// Build the codec
+		encodingConfig := parseConfig.GetEncodingConfigBuilder()()
+		
+		// Get the db	
+		databaseCtx := database.NewContext(cfg.Database, &encodingConfig, parseConfig.GetLogger())
+		db, err := parseConfig.GetDBBuilder()(databaseCtx)
+		if err != nil {
+			return fmt.Errorf("Error while getting the db: %s", err)
+		}
+		limit := config.Cfg.Database.Limit
 		offset := int64(0)
+		
 
 		for {
 			// SELECT rows from transaction_old table
 			txRows, err := db.SelectRows(limit, offset)
 			if err != nil {
-				log.Fatal("error while selecting transaction rows: ", err)
-				return
+				return fmt.Errorf("error while selecting transaction rows: ", err)
 			}
 			if len(txRows) == 0 {
 				break
@@ -29,29 +47,25 @@ func MigrateTablesCmd() *cobra.Command {
 			// INSERT INTO transaction and message tables
 			err = db.InsertTransactions(txRows)
 			if err != nil {
-				log.Fatal("error while inserting data: ", err)
-				return
+				return fmt.Errorf("error while inserting data: ", err)
 			}
 
 			offset += limit
 		}
 
 		// DROP old messages_by_address function
-		err := db.DropMessageByAddressFunc()
+		err = db.DropMessageByAddressFunc()
 		if err != nil {
-			log.Fatal("error while dropping messages_by_address function: ", err)
-			return
+				return fmt.Errorf("error while dropping messages_by_address function: ", err)
 		}
 
 		// CREATE new messages_by_address function
 		err = db.CreateMessageByAddressFunc()
 		if err != nil {
-			log.Fatal("error while creating messages_by_address function: ", err)
-			return
+				return fmt.Errorf("error while creating messages_by_address function: ", err)
 		}
 
 		fmt.Println("--- Migration completed ---")
-		return
 
 			return nil
 		},
