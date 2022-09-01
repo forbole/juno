fd18aa7f8f53db3595abddbdb00cf0a17cd701a5
huichiaotsou <huichiao.tsou@gmail.com>
2022-09-01 21:09:44 +0800

    add comment

--- types/config/utils.go
+++ types/config/utils.go
@@ -14,10 +14,11 @@ func GetConfigFilePath() string {
 	return path.Join(HomePath, "config.yaml")
 }
 
+// GetAvgBlockTime returns the average_block_time in the configuration file or
+// returns 3 seconds if it is not configured
 func GetAvgBlockTime() time.Duration {
 	if Cfg.Parser.AvgBlockTime == nil {
 		return 3 * time.Second
 	}
-
 	return *Cfg.Parser.AvgBlockTime
 }
