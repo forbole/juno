a59db1d542bc6ce42ce2b4a793f7413f097c512a
Aaron <76254323+huichiaotsou@users.noreply.github.com>
2022-09-02 17:23:23 +0800

    fix: re-enqueue failed block after average block time (#73)
    ## Description
    
    request from Ravi: https://forbole.atlassian.net/browse/BDU-490
    
    ## Checklist
    - [x] Targeted PR against correct branch.
    - [ ] Linked to Github issue with discussion and accepted design OR link to spec that describes this work.
    - [ ] Wrote unit tests.  
    - [x] Re-reviewed `Files changed` in the Github PR explorer.

--- types/config/utils.go
+++ types/config/utils.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"path"
+	"time"
 )
 
 var (
@@ -11,4 +12,13 @@ var (
 // GetConfigFilePath returns the path to the configuration file given the executable name
 func GetConfigFilePath() string {
 	return path.Join(HomePath, "config.yaml")
+}
+
+// GetAvgBlockTime returns the average_block_time in the configuration file or
+// returns 3 seconds if it is not configured
+func GetAvgBlockTime() time.Duration {
+	if Cfg.Parser.AvgBlockTime == nil {
+		return 3 * time.Second
+	}
+	return *Cfg.Parser.AvgBlockTime
 }
