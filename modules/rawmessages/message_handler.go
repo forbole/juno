b1d51e8d8f8670509fde360ce579acbf06387583
Magic Cat <pusz.monika@gmail.com>
2022-09-23 18:21:43 +0700

    add message handler

---
+++ modules/rawmessages/message_handler.go
@@ -0,0 +1,50 @@
+package rawmessages
+
+import (
+	"fmt"
+	"regexp"
+
+	// "strings"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/forbole/juno/v3/database"
+	"github.com/forbole/juno/v3/types"
+)
+
+// HandleMsg represents a message handler that stores the given message inside the proper database table
+func HandleMsg(
+	index int, msg *codectypes.Any, tx *types.Tx, cdc codec.Codec, db database.Database,
+) error {
+
+	var msgData sdk.MsgData
+	var addresses [][]string
+	var involvedAddresses []string
+
+	// Unmarshal the value properly
+	err := cdc.Unmarshal(msg.Value, &msgData)
+	if err != nil {
+		return fmt.Errorf("error when unmarshaling msg %s", err)
+	}
+
+	addressRegex := regexp.MustCompile("osmo[0-9a-zA-Z]+")
+	if addressRegex.MatchString(string(msgData.Data)) {
+		addresses = addressRegex.FindAllStringSubmatch(string(msgData.Data), -1)
+	}
+
+	// rewrite into 1d array
+	for _, addr := range addresses {
+		involvedAddresses = append(involvedAddresses, addr...)
+	}
+
+	return db.SaveMessage(types.NewMessage(
+		tx.TxHash,
+		index,
+		msgData.MsgType,
+		string(msg.Value),
+		involvedAddresses,
+		tx.Height,
+	))
+}
