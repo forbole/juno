27c1d8d9be7ed56aeff1c474d983bc9e0a8bf2d8
Magic Cat <pusz.monika@gmail.com>
2022-09-27 18:38:50 +0700

    updated types

--- types/cosmos.go
+++ types/cosmos.go
@@ -137,13 +137,13 @@ type Message struct {
 	TxHash    string
 	Index     int
 	Type      string
-	Value     sdk.MsgData
+	Value     string
 	Addresses []string
 	Height    int64
 }
 
 // NewMessage allows to build a new Message instance
-func NewMessage(txHash string, index int, msgType string, value sdk.MsgData, addresses []string, height int64) *Message {
+func NewMessage(txHash string, index int, msgType string, value string, addresses []string, height int64) *Message {
 	return &Message{
 		TxHash:    txHash,
 		Index:     index,
