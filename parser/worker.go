84c447dcc10b93d6af56dfe4dcdcd76ea3f74430
Aaron <76254323+huichiaotsou@users.noreply.github.com>
2022-11-25 23:02:14 +0800

    fix: add wait group to handle message (#77)
    ## Description
    
    
    This PR adds wait group to handle messages concurrently. 
    
    Background: 
    When I used `parse blocks`cmd to parse 1 specific height that has transactions & messages,  the messages can be lost because of being handled through go routines and when the main program is done, all go routines are closed. 
    
    ## Checklist
    - [x] Targeted PR against correct branch.
    - [ ] Linked to Github issue with discussion and accepted design OR link to spec that describes this work.
    - [ ] Wrote unit tests.  
    - [x] Re-reviewed `Files changed` in the Github PR explorer.

--- parser/worker.go
+++ parser/worker.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/x/authz"
@@ -327,18 +328,23 @@ func (w Worker) handleMessage(index int, msg sdk.Msg, tx *types.Tx) {
 // ExportTxs accepts a slice of transactions and persists then inside the database.
 // An error is returned if the write fails.
 func (w Worker) ExportTxs(txs []*types.Tx) error {
-	// handle all transactions inside the block
+	var wg sync.WaitGroup
+
 	for _, tx := range txs {
-		// save the transaction
+		// Save the transaction
 		err := w.saveTx(tx)
 		if err != nil {
 			return fmt.Errorf("error while storing txs: %s", err)
 		}
 
-		// call the tx handlers
-		go w.handleTx(tx)
+		// Handle the transaction concurrently
+		wg.Add(1)
+		go func(tx *types.Tx) {
+			defer wg.Done()
+			w.handleTx(tx)
+		}(tx)
 
-		// handle all messages contained inside the transaction
+		// Parse all the messages contained inside the transaction
 		sdkMsgs := make([]sdk.Msg, len(tx.Body.Messages))
 		for i, msg := range tx.Body.Messages {
 			var stdMsg sdk.Msg
@@ -349,11 +355,18 @@ func (w Worker) ExportTxs(txs []*types.Tx) error {
 			sdkMsgs[i] = stdMsg
 		}
 
-		// call the msg handlers
+		// Handle all the messages concurrently
 		for i, sdkMsg := range sdkMsgs {
-			go w.handleMessage(i, sdkMsg, tx)
+			wg.Add(1)
+			go func(i int, sdkMsg sdk.Msg) {
+				defer wg.Done()
+				w.handleMessage(i, sdkMsg, tx)
+			}(i, sdkMsg)
 		}
 	}
+
+	// Wait all transactions and messages to be parsed
+	wg.Wait()
 
 	totalBlocks := w.db.GetTotalBlocks()
 	logging.DbBlockCount.WithLabelValues("total_blocks_in_db").Set(float64(totalBlocks))
