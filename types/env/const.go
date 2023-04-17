97676ac49f4168bf631bd062990c9d53912a1489
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2023-04-17 20:38:20 +0800

    Update types/env/const.go
    Co-authored-by: Riccardo <riccardo.montagnin@gmail.com>

--- types/env/const.go
+++ types/env/const.go
@@ -2,8 +2,8 @@ package env
 
 const (
 	DatabaseURI           = "JUNO_DATABASE_URL"
-	DatabaseSSLModeEnable = "false"
-	DatabaseSSLRootCert   = ""
-	DatabaseSSLCert       = ""
-	DatabaseSSLKey        = ""
+	DatabaseSSLModeEnable = "JUNO_DATABASE_SSL_MODE_ENABLED"
+	DatabaseSSLRootCert   = "JUNO_DATABASE_SSL_ROOT_CERT"
+	DatabaseSSLCert       = "JUNO_DATABASE_SSL_CERT"
+	DatabaseSSLKey        = "JUNO_DATABASE_SSL_KEY"
 )
