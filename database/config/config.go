be854f5e0582384d3859b809d445ebeb5c59ff50
Magic Cat <37407870+monikacat@users.noreply.github.com>
2023-04-17 20:03:22 -0500

    feat: add TSL support when connecting to database (#94)
    <!-- < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < ☺
    v                               ✰  Thanks for creating a PR! ✰
    v    Before smashing the submit button please review the checkboxes.
    v If a checkbox is n/a - please still include it but + a little note why
    ☺ > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >  -->
    
    ## Description
    <!-- Small description -->
    
    [BDU-494](https://forbole.atlassian.net/jira/software/c/projects/BDU/issues/BDU-494)
    
    ## Checklist
    - [x] Targeted PR against correct branch.
    - [x] Linked to Github issue with discussion and accepted design OR link
    to spec that describes this work.
    - [x] Wrote unit tests.
    - [x] Re-reviewed `Files changed` in the Github PR explorer.
    
    [BDU-494]:
    https://forbole.atlassian.net/browse/BDU-494?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: Riccardo <riccardo.montagnin@gmail.com>

--- database/config/config.go
+++ database/config/config.go
@@ -8,6 +8,10 @@ type Config struct {
 	MaxIdleConnections int    `yaml:"max_idle_connections"`
 	PartitionSize      int64  `yaml:"partition_size"`
 	PartitionBatchSize int64  `yaml:"partition_batch"`
+	SSLModeEnable      string `yaml:"ssl_mode_enable"`
+	SSLRootCert        string `yaml:"ssl_root_cert"`
+	SSLCert            string `yaml:"ssl_cert"`
+	SSLKey             string `yaml:"ssl_key"`
 }
 
 func (c *Config) getURL() *url.URL {
@@ -44,7 +48,7 @@ func (c *Config) GetSSLMode() string {
 }
 
 func NewDatabaseConfig(
-	url string,
+	url, sslModeEnable, sslRootCert, sslCert, sslKey string,
 	maxOpenConnections int, maxIdleConnections int,
 	partitionSize int64, batchSize int64,
 ) Config {
@@ -54,6 +58,10 @@ func NewDatabaseConfig(
 		MaxIdleConnections: maxIdleConnections,
 		PartitionSize:      partitionSize,
 		PartitionBatchSize: batchSize,
+		SSLModeEnable:      sslModeEnable,
+		SSLRootCert:        sslRootCert,
+		SSLCert:            sslCert,
+		SSLKey:             sslKey,
 	}
 }
 
@@ -61,6 +69,10 @@ func NewDatabaseConfig(
 func DefaultDatabaseConfig() Config {
 	return NewDatabaseConfig(
 		"postgresql://user:password@localhost:5432/database-name?sslmode=disable&search_path=public",
+		"false",
+		"",
+		"",
+		"",
 		1,
 		1,
 		100000,
