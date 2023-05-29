0e3523dd752b81bd07f508a40b73c945a2dbbaa3
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-05-29 15:25:53 -0500

    build: bump Go package to github.com/forbole/juno/v5

--- parser/worker.go
+++ parser/worker.go
@@ -7,21 +7,22 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/x/authz"
 
-	"github.com/forbole/juno/v4/logging"
+	"github.com/forbole/juno/v5/logging"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 
-	"github.com/forbole/juno/v4/database"
-	"github.com/forbole/juno/v4/types/config"
+	"github.com/forbole/juno/v5/database"
+	"github.com/forbole/juno/v5/types/config"
+
+	"github.com/forbole/juno/v5/modules"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/forbole/juno/v4/modules"
 	tmctypes "github.com/tendermint/tendermint/rpc/core/types"
 	tmtypes "github.com/tendermint/tendermint/types"
 
-	"github.com/forbole/juno/v4/node"
-	"github.com/forbole/juno/v4/types"
-	"github.com/forbole/juno/v4/types/utils"
+	"github.com/forbole/juno/v5/node"
+	"github.com/forbole/juno/v5/types"
+	"github.com/forbole/juno/v5/types/utils"
 )
 
 // Worker defines a job consumer that is responsible for getting and
