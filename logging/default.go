472e64a6cac00698b2aaf4036d93332d2b7c502f
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-05-29 15:25:53 -0500

    build: bump Go package to github.com/forbole/juno/v5

--- logging/default.go
+++ logging/default.go
@@ -11,8 +11,8 @@ import (
 	"github.com/rs/zerolog/log"
 	tmctypes "github.com/tendermint/tendermint/rpc/core/types"
 
-	"github.com/forbole/juno/v4/modules"
-	"github.com/forbole/juno/v4/types"
+	"github.com/forbole/juno/v5/modules"
+	"github.com/forbole/juno/v5/types"
 )
 
 var (
