f9cdbd83955da62bfdba37c7a9ab755fda5850e1
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-05-31 10:16:55 -0500

    fix: fix lint errors

--- cmd/migrate/v3/utils.go
+++ cmd/migrate/v3/utils.go
@@ -2,7 +2,6 @@ package v3
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 
@@ -20,7 +19,7 @@ func GetConfig() (Config, error) {
 		return Config{}, fmt.Errorf("config file does not exist")
 	}
 
-	bz, err := ioutil.ReadFile(file)
+	bz, err := os.ReadFile(file)
 	if err != nil {
 		return Config{}, fmt.Errorf("error while reading config files: %s", err)
 	}
