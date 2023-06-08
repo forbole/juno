5040d27471eea74ccfaf51d7bf9a3e28ff9083d6
Magic Cat <pusz.monika@gmail.com>
2023-06-08 16:14:47 +0700

    update authz message parser to handle gov v1 and v1beta1 msgs

--- modules/messages/account_parser.go
+++ modules/messages/account_parser.go
@@ -301,8 +301,15 @@ func AuthzMessageParser(c codec.Codec, cosmosMsg sdk.Msg) ([]string, error) {
 				}
 				addresses = append(addresses, msg.Grantee)
 				return addresses, nil
-			case strings.Contains(index.TypeUrl, "gov"):
-				addresses, err := GovMessagesParser(c, executedMsg)
+			case strings.Contains(index.TypeUrl, "gov.v1beta1"):
+				addresses, err := GovV1Beta1MessagesParser(c, executedMsg)
+				if err != nil {
+					return nil, MessageNotSupported(executedMsg)
+				}
+				addresses = append(addresses, msg.Grantee)
+				return addresses, nil
+			case strings.Contains(index.TypeUrl, "gov.v1."):
+				addresses, err := GovV1MessageParser(c, executedMsg)
 				if err != nil {
 					return nil, MessageNotSupported(executedMsg)
 				}
