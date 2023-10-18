5896c30bb1af6d75390b582f491b3c023853dd3e
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-10-18 11:17:45 -0500

    feat: added back JoinMessageParser function

--- modules/messages/utils.go
+++ modules/messages/utils.go
@@ -1,6 +1,10 @@
 package messages
 
-import "unicode/utf8"
+import (
+	"unicode/utf8"
+
+	"github.com/forbole/juno/v4/types"
+)
 
 func TrimLastChar(s string) string {
 	r, size := utf8.DecodeLastRuneInString(s)
@@ -8,4 +12,20 @@ func TrimLastChar(s string) string {
 		size = 0
 	}
 	return s[:len(s)-size]
+}
+
+// JoinMessageParsers joins together all the given parsers, calling them in order
+func JoinMessageParsers(parsers ...MessageAddressesParser) MessageAddressesParser {
+	return func(tx *types.Tx) ([]string, error) {
+		for _, parser := range parsers {
+			// Try getting the addresses
+			addresses, _ := parser(tx)
+
+			// If some addresses are found, return them
+			if len(addresses) > 0 {
+				return addresses, nil
+			}
+		}
+		return DefaultMessagesParser(tx)
+	}
 }
