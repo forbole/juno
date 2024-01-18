e3f352f5a9644d08f17a8d166d438ad6e04393c3
Magic Cat <pusz.monika@gmail.com>
2024-01-18 16:35:36 +0700

    updated v5 migrator

--- cmd/migrate/v5/migrate.go
+++ cmd/migrate/v5/migrate.go
@@ -2,16 +2,13 @@ package v5
 
 import (
 	"fmt"
-	"os"
 
-	"gopkg.in/yaml.v3"
 	utils "github.com/forbole/juno/v5/cmd/migrate/utils"
 
 	parse "github.com/forbole/juno/v5/cmd/parse/types"
 	"github.com/forbole/juno/v5/database"
 	v5db "github.com/forbole/juno/v5/database/legacy/v5"
 	"github.com/forbole/juno/v5/database/postgresql"
-	"github.com/forbole/juno/v5/types/config"
 )
 
 // RunMigration runs the migrations from v4 to v5
@@ -19,22 +16,6 @@ func RunMigration(parseConfig *parse.Config) error {
 	cfg, err := GetConfig()
 	if err != nil {
 		return fmt.Errorf("error while reading config: %s", err)
-	}
-
-	// Refresh the global configuration
-	err = parse.UpdatedGlobalCfg(parseConfig)
-	if err != nil {
-		return err
-	}
-
-	bz, err := yaml.Marshal(&cfg)
-	if err != nil {
-		return fmt.Errorf("error while serializing config: %s", err)
-	}
-
-	err = os.WriteFile(config.GetConfigFilePath(), bz, 0666)
-	if err != nil {
-		return fmt.Errorf("error while writing v5 config: %s", err)
 	}
 
 	// Migrate the database
